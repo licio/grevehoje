8dc8c261e26aa96765780b444e50f50c9722c2ad
Licio Fonseca <liciofernando@gmail.com>
2012-09-03 16:57:59 -0300

    first commit, duh

---
+++ ehositeousoueu.py
@@ -0,0 +1,28 @@
+import cgi
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    import os
+    from google.appengine.ext.webapp import template
+    template_values = {
+	'legal': 'nada',
+       }
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+    
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                      ],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
